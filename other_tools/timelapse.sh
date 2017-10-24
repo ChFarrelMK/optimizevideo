030f35548867ef56a81b2b39c65c333a72fbf0f3
Andreas Wenzel <anwen@me.com>
2017-10-24 12:51:52 +0200

    Removed old tests

--- other_tools/timelapse.sh
+++ other_tools/timelapse.sh
@@ -20,11 +20,6 @@ function generateStills() {
     do
         pad="${zeroes}${NUM}"
         ffmpeg -i "${X}" -r 1 -f image2 -vsync cfr "${DIR}.ffmpeg_temp"/${pad:(-9)}%05d.png
-        #ffmpeg -i "${X}" -vf "select=eq(pict_type\,I)" -vsync cfr "${DIR}.ffmpeg_temp"/${pad:(-9)}%05d.png
-        #ffmpeg -i "${X}" -vf "select=eq(pict_type\,I)" -vsync vfr -vf "drawtext=fontfile=/Library/Fonts/Tahoma.ttf: timecode='00\:00\:00\:00': r=25: x=(w-tw)/2: y=h-(2*lh): fontcolor=white: box=1: boxcolor=0x00000000@1" "${DIR}.ffmpeg_temp"/${pad:(-9)}%05d.png
-        #ffmpeg -i "${X}" -vf "select=eq(pict_type\,I)" -vsync vfr -vf "drawtext=fontfile=/Library/Fonts/Tahoma.ttf :expansion=normal: text=%{metadata\\\\:creation_time}: \ x=(w-tw)/2: y=h-(2*lh): fontcolor=white@0.8" "${DIR}.ffmpeg_temp"/${pad:(-9)}%05d.png
-        #ffmpeg -i "${X}" -vf "select=eq(pict_type\,I)" -vsync vfr -strftime 1 "%Y-%m-%d_%H-%M-%S.png"
-        #ffmpeg -i "${X}" -r 1 -f image2 -vsync cfr -vf "drawtext='fontfile=/Library/Fonts/Tahoma.ttf': text='%{metadata\: creation_time}': x=10: y=10: fontcolor=white@0.5: box=1: boxcolor=0x00000000@0.5" "${DIR}.ffmpeg_temp"/${pad:(-9)}%05d.png
         (( NUM = NUM + 1 ))
     done
 
@@ -71,9 +66,6 @@ do
         generateStills "${DIR}"
     fi
 
-    #ffmpeg -i ffmpeg_temp/%014d.png -q:v 0 test.${Extension}
-    #ffmpeg -f image2 -r 25 -i ffmpeg_temp/%014d.png -vcodec libx264 -crf 22 test.${Extension}
-    #ffmpeg -i ffmpeg_temp/%014d.png -framerate 25 -pix_fmt yuv420p -q:v 0 test.${Extension}
     if [ ! -e "${DIR}".${Extension} ]
     then
         ffmpeg -pattern_type glob -i "${DIR}.ffmpeg_temp"'/*.png' -c:v libx264 "${DIR}".${Extension}
@@ -95,5 +87,3 @@ do
 done
 
 exit 0
-
-#ffmpeg -y -i S1480002.MP4 -vf "drawtext=fontfile=arial.ttf :expansion=normal: text=%{metadata\\:creation_time}: \ x=(w-tw)/2: y=h-(2*lh): fontcolor=white@0.8" output.mp4
